60d1158d99f1996fa984ee7f688fb8486d9780ce
jjakkc <7483411+jjakkc@users.noreply.github.com>
2021-01-22 14:55:56 -0500

    Fix query bug in sp for getting available room types

--- SQLHotelDB/dbo/spRoomTypes_GetAvailableTypes.sql
+++ SQLHotelDB/dbo/spRoomTypes_GetAvailableTypes.sql
@@ -8,11 +8,11 @@ BEGIN
 	SELECT [rt].[Id], [rt].[Title], [rt].[Description], [rt].[Price]
 	FROM dbo.Rooms r
 	inner join dbo.RoomTypes rt ON rt.Id = r.RoomTypeId
-	WHERE r.id not in (
+	WHERE r.Id not in (
 		--Querying rooms that are booked based on date range
 		SELECT b.RoomId
 		FROM dbo.Bookings b
 		WHERE (@startDate <= b.EndDate and @endDate >= b.StartDate)
 	)
-	GROUP BY rt.Title, rt.Description, rt.Price;
+	GROUP BY rt.Id, rt.Title, rt.Description, rt.Price;
 END
