26b7a3283728cbf6df61b4e45c5c5f15e7d95f9f
jjakkc <7483411+jjakkc@users.noreply.github.com>
2021-01-30 22:25:32 -0500

    Add razor page for searching available rooms.

---
+++ HotelApp.Web/Pages/RoomSearch.cshtml.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using HotelAppLibrary.Data;
+using HotelAppLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HotelApp.Web.Pages
+{
+    public class RoomSearchModel : PageModel
+    {
+        private readonly IDatabaseData _db;
+
+        [BindProperty(SupportsGet = true)]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        [BindProperty(SupportsGet = true)]
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; } = DateTime.Now.AddDays(1);
+
+        [BindProperty(SupportsGet = true)]
+        public bool SearchEnabled { get; set; } = false;
+        
+        [BindProperty]
+        public List<RoomType> AvailableRoomTypes { get; set; }
+
+        public RoomSearchModel(IDatabaseData db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            if (SearchEnabled)
+            {
+                AvailableRoomTypes = _db.GetAvailableRoomTypes(StartDate, EndDate);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage(new { SearchEnabled = true, StartDate, EndDate});
+        }
+    }
+}
